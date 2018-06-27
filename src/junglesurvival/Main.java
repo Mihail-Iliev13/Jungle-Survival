098d3b394e87929fb8079e399fd23416c8e369be
vladimirVenkov <vladimir.ognyanov.venkov@gmail.com>
2018-06-27 17:27:56 +0300

    changes in Participant

--- src/junglesurvival/Main.java
+++ src/junglesurvival/Main.java
@@ -3,7 +3,7 @@ package junglesurvival;
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println("hello");
 
     }
 }
--- src/junglesurvival/participants/Participant.java
+++ src/junglesurvival/participants/Participant.java
@@ -1,5 +1,14 @@
 package junglesurvival.participants;
 
 public class Participant {
-  
+    private String name;
+    private  int lifepoints;
+    private  int attack;
+
+
+    public Participant(String name, int lifepoints, int attack) {
+        this.name = name;
+        this.lifepoints = lifepoints;
+        this.attack = attack;
+    }
 }
