e3701791db082f43d167851f51ae017955762101
vladimirVenkov <vladimir.ognyanov.venkov@gmail.com>
2018-06-27 22:10:49 +0300

    JungleTEST Class didn't work... Moving test to Main Class.

--- src/junglesurvival/Main.java
+++ src/junglesurvival/Main.java
@@ -1,9 +1,29 @@
 package junglesurvival;
 
+import junglesurvival.Items.Food;
+import junglesurvival.Items.Jewel;
+import junglesurvival.Items.Weapon;
+import junglesurvival.participants.Gender;
+import junglesurvival.participants.Hero;
+import junglesurvival.participants.Monster;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello");
-
+        Hero opra = new Hero("Opra", 100, 30, Gender.FEMALE);
+        Monster stryvnik = new Monster("ManEater", 40, 10,70);
+        Weapon bow = new Weapon("GreatBow");
+        Jewel diamond = new Jewel("BigDiamond");
+        Food mushroom = new Food("Muhomorka", false, 40);
+        opra.attackEnemy(stryvnik);
+        stryvnik.attack(opra);
+        opra.status();
+        System.out.println("stryvnik has following lifepoints: " + stryvnik.getLifepoints());
+        mushroom.beingConsumed(opra);
+        opra.attackEnemy(stryvnik);
+        opra.pickItem(bow);
+        opra.pickItem(diamond);
+        opra.status();
     }
 }
--- src/junglesurvival/participants/Hero.java
+++ src/junglesurvival/participants/Hero.java
@@ -7,10 +7,6 @@ import junglesurvival.Items.Weapon;
 import java.util.ArrayList;
 import java.util.List;
 
-enum Gender {
-    MALE, FEMALE
-}
-
 public class Hero extends Participant {
     private Gender gender;
     private int experience;
@@ -25,13 +21,13 @@ public class Hero extends Participant {
         bribAbility = new ArrayList<>();
         currentAttack = super.getAttack();
     }
-    void pickItem(Item item) {
+    public void pickItem(Item item) {
         bag.add(item);
         if(item instanceof Weapon) currentAttack ++; //TODO change when weapon is finished!!!
         if(item instanceof Jewel) bribAbility.add((Jewel)item);
     }
 
-    void attackEnemy(Enemy enemy) {
+    public void attackEnemy(Enemy enemy) {
     enemy.setLifepoints(enemy.getLifepoints() - currentAttack);
 
     if(enemy.getLifepoints() <= 0) killingEnemy(enemy);
