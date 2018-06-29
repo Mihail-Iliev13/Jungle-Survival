25f13ea05801709cfec09aa6e86624c9610138ea
vladimirVenkov <vladimir.ognyanov.venkov@gmail.com>
2018-06-29 18:26:25 +0300

    adding Angry Slicer

---
+++ src/junglesurvival/participants/AngrySlicer.java
@@ -0,0 +1,32 @@
+package junglesurvival.participants;
+
+import junglesurvival.Dice;
+
+public class AngrySlicer extends Boss {
+
+    private static final int SLICER_LIFE_POINTS= 85;
+    private static final int SLICER_ATTACK_POINTS = 12;
+    private static final int GIVEN_EXPERIENCE = 75;
+
+    public AngrySlicer() {
+        super("Angry Slicer");
+        setLifepoints(SLICER_LIFE_POINTS);
+        setAttack(SLICER_ATTACK_POINTS);
+        setGivenExperience(GIVEN_EXPERIENCE);
+    }
+
+    @Override
+    void specialAttack() {
+        Dice dice = new Dice();
+        int diceValue = dice.getValue() / 3 ;
+        System.out.println("Careful! Angry Slicer Attack is increasing!");
+        setAttack(getAttack() + diceValue);
+    }
+
+    @Override
+    public void attack(Hero hero) {
+        int finalAttack = this.getAttack();
+        hero.setLifepoints(hero.getLifepoints() - finalAttack);
+        specialAttack();
+    }
+}
