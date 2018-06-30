b8fe57b8e8ab2db8c88d1a91d6510e4a19ee7f93
Mihail Iliev <mihaililiev13@gmail.com>
2018-06-30 17:36:46 +0300

    added class Animal and enum AnimalType

---
+++ src/junglesurvival/participants/Animal.java
@@ -0,0 +1,33 @@
+package junglesurvival.participants;
+
+import com.sun.org.apache.regexp.internal.RE;
+
+public class Animal extends Enemy {
+
+  private static final int REGULAR_DAMAGE_PER_ATTACK = 1;
+  private static final int ANIMAL_STARTING_LIFE_POINTS = 5;
+  private static final int ANIMAL_GIVEN_EXPERIENCE = 1;
+  private AnimalType animal;
+
+  public Animal(AnimalType animal) {
+    super(animal.getName());
+    this.animal = animal;
+    setGivenExperience(ANIMAL_GIVEN_EXPERIENCE);
+    setAttack(REGULAR_DAMAGE_PER_ATTACK);
+    setLifepoints(ANIMAL_STARTING_LIFE_POINTS);
+  }
+
+  private AnimalType getAnimalType() {
+    return animal;
+  }
+
+  public void makeSound(){
+    System.out.printf("I am %s and I can %s.\n", getName(), getAnimalType().getSound());
+  }
+
+  @Override
+  public void attack(Hero hero) {
+    int heroCurrentLifePoints = hero.getLifepoints();
+    hero.setLifepoints(heroCurrentLifePoints - getAttack());
+  }
+}
